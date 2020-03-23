1f4d27e2df907ea5e51383f4af852b2e2cb54586
João Ventura <joaojonesventura@gmail.com>
2020-03-23 11:40:07 +0000

    Compile pybridge with Python 3.8.2

--- PyApp/app/src/main/jni/Android.mk
+++ PyApp/app/src/main/jni/Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
-CRYSTAX_PATH := /Users/jventura/Library/Android/crystax-ndk-10.3.2
+RECIPES_PATH := $(LOCAL_PATH)/../../../../../recipes
+PYTHON_PATH := $(RECIPES_PATH)/python3.8.2/Python-3.8.2
 
 
 # Build libpybridge.so
@@ -8,14 +9,14 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := pybridge
 LOCAL_SRC_FILES := pybridge.c
 LOCAL_LDLIBS := -llog
-LOCAL_SHARED_LIBRARIES := python3.5m
+LOCAL_SHARED_LIBRARIES := python3.8
 include $(BUILD_SHARED_LIBRARY)
 
 
-# Include libpython3.5m.so
+# Include libpython3.8.so
 
 include $(CLEAR_VARS)
-LOCAL_MODULE    := python3.5m
-LOCAL_SRC_FILES := $(CRYSTAX_PATH)/sources/python/3.5/libs/$(TARGET_ARCH_ABI)/libpython3.5m.so
-LOCAL_EXPORT_CFLAGS := -I $(CRYSTAX_PATH)/sources/python/3.5/include/python/
+LOCAL_MODULE    := python3.8
+LOCAL_SRC_FILES := $(PYTHON_PATH)/libpython3.8.so
+LOCAL_EXPORT_CFLAGS := -I $(PYTHON_PATH) -I $(PYTHON_PATH)/Include
 include $(PREBUILT_SHARED_LIBRARY)
